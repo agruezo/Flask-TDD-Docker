2aea01e62c9454620408e05772cf004bf7a7292c
agruezo <agruezo@gmail.com>
2022-11-02 05:55:04 -0400

    add tests

--- src/config.py
+++ src/config.py
@@ -17,9 +17,9 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    url = os.environ.get("DATABASE_URL")
+    sa_url = os.environ.get("DATABASE_URL")
 
-    if url is not None and url.startswith("postgres://"):
-        url = url.replace("postgres://", "postgresql://", 1)
+    if sa_url is not None and sa_url.startswith("postgres://"):
+        sa_url = sa_url.replace("postgres://", "postgresql://", 1)
 
-    SQLALCHEMY_DATABASE_URI = url
+    SQLALCHEMY_DATABASE_URI = sa_url
